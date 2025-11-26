0ffc8de96dbaf017ee6ea2089e12b33e370d6727
vinaykaladeep <vinaykaladeep1@gmail.com>
2025-11-26 23:54:02 +0530

    Data ingestion component completed

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 # below code is to check the logging config
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
 # --------------------------------------------------------------------------------
 
 # # below code is to check the exception config
@@ -20,3 +20,8 @@ logging.critical("This is a critical message.")
 #     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
