f2b345ea22cb6ef524d2f146a09e4b0b62a1a6b6
vinaykaladeep <vinaykaladeep1@gmail.com>
2025-12-20 22:53:00 +0530

    github actions,docker, ECR included

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Vindpop",
+    author_email="xyz@gmail.com",
     packages=find_packages()
 )
